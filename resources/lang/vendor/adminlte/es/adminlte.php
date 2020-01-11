16080af50a4e449699c863e20906b7d8ca5ea56d
Carlos Ferreira <carlos.ferreira@inevent.com>
2020-01-11 15:53:51 -0300

    Install and Config AdminLTE

---
+++ resources/lang/vendor/adminlte/es/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nombre completo',
+    'email'                       => 'Email',
+    'password'                    => 'Contraseña',
+    'retype_password'             => 'Confirmar la contraseña',
+    'remember_me'                 => 'Recordarme',
+    'register'                    => 'Registrarse',
+    'register_a_new_membership'   => 'Crear una nueva cuenta',
+    'i_forgot_my_password'        => 'Olvidé mi contraseña',
+    'i_already_have_a_membership' => 'Ya tengo una cuenta',
+    'sign_in'                     => 'Acceder',
+    'log_out'                     => 'Salir',
+    'toggle_navigation'           => 'Navegación de palanca',
+    'login_message'               => 'Autenticarse para iniciar sesión',
+    'register_message'            => 'Crear una nueva cuenta',
+    'password_reset_message'      => 'Restablecer la contraseña',
+    'reset_password'              => 'Restablecer la contraseña',
+    'send_password_reset_link'    => 'Enviar enlace de restablecimiento de contraseña',
+];
