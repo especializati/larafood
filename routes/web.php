c4a7434d43eced04b2ab584677d17b9647e56267
Carlos Ferreira <carlos.ferreira@inevent.com>
2020-02-06 10:42:28 -0300

    Organized Routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
-Route::put('admin/plans/{url}', 'Admin\PlanController@update')->name('plans.update');
-Route::get('admin/plans/{url}/edit', 'Admin\PlanController@edit')->name('plans.edit');
-Route::any('admin/plans/search', 'Admin\PlanController@search')->name('plans.search');
-Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plans.destroy');
-Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
-Route::post('admin/plans', 'Admin\PlanController@store')->name('plans.store');
-Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+Route::prefix('admin')
+        ->namespace('Admin')
+        ->group(function() {
 
-Route::get('admin', 'Admin\PlanController@index')->name('admin.index');
+    Route::get('plans/create', 'PlanController@create')->name('plans.create');
+    Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
+    Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
+    Route::any('plans/search', 'PlanController@search')->name('plans.search');
+    Route::delete('plans/{url}', 'PlanController@destroy')->name('plans.destroy');
+    Route::get('plans/{url}', 'PlanController@show')->name('plans.show');
+    Route::post('plans', 'PlanController@store')->name('plans.store');
+    Route::get('plans', 'PlanController@index')->name('plans.index');
+
+    Route::get('/', 'PlanController@index')->name('admin.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
