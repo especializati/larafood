449bfce7194f2817edb2f3b31a2be87dd8e0f4a0
Carlos Ferreira <conta@especializati.com.br>
2020-04-30 16:23:04 -0300

    Tests Laravel: Tenant API

---
+++ tests/Feature/TenantTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tenant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TenantTest extends TestCase
+{
+    /**
+     * Test Get All Tenants
+     *
+     * @return void
+     */
+    public function testGetAllTenants()
+    {
+        factory(Tenant::class, 10)->create();
+
+        $response = $this->getJson('/api/v1/tenants');
+
+        $response->assertStatus(200)
+                    ->assertJsonCount(10, 'data');
+    }
+
+    /**
+     * Test Get Error Single Tenant
+     *
+     * @return void
+     */
+    public function testErrorGetTenant()
+    {
+        $tenant = 'fake_value';
+
+        $response = $this->getJson("/api/v1/tenants/{$tenant}");
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test Get Error Single Tenant
+     *
+     * @return void
+     */
+    public function testGetTenantByIdentify()
+    {
+        $tenant = factory(Tenant::class)->create();
+
+        $response = $this->getJson("/api/v1/tenants/{$tenant->uuid}");
+
+        $response->assertStatus(200);
+    }
+}
