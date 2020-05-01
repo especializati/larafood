4416344f13fc1d834a9eda50462aebe06540df22
Carlos Ferreira <conta@especializati.com.br>
2020-05-01 16:09:08 -0300

    Tests Laravel: Auth me and logout

--- tests/Feature/Api/AuthTest.php
+++ tests/Feature/Api/AuthTest.php
@@ -63,4 +63,56 @@ class AuthTest extends TestCase
         $response->assertStatus(200)
                     ->assertJsonStructure(['token']);
     }
+
+    /**
+     * Error Get Me
+     *
+     * @return void
+     */
+    public function testErrorGetMe()
+    {
+        $response = $this->getJson('/api/auth/me');
+
+        $response->assertStatus(401);
+    }
+
+    /**
+     * Get Me
+     *
+     * @return void
+     */
+    public function testGetMe()
+    {
+        $client = factory(Client::class)->create();
+        $token = $client->createToken(Str::random(10))->plainTextToken;
+
+        $response = $this->getJson('/api/auth/me', [
+            'Authorization' => "Bearer {$token}",
+        ]);
+
+        $response->assertStatus(200)
+                    ->assertExactJson([
+                        'data' => [
+                            'name' => $client->name,
+                            'email' => $client->email,
+                        ]
+                    ]);
+    }
+
+    /**
+     * Logout
+     *
+     * @return void
+     */
+    public function testLogout()
+    {
+        $client = factory(Client::class)->create();
+        $token = $client->createToken(Str::random(10))->plainTextToken;
+
+        $response = $this->postJson('/api/auth/logout', [], [
+            'Authorization' => "Bearer {$token}",
+        ]);
+
+        $response->assertStatus(204);
+    }
 }
