6c6a5fdabf2d4e89f76d5445cb6c2c76d01634db
Carlos Ferreira <conta@especializati.com.br>
2020-05-20 09:57:08 -0300

    Fix Validation New Tenant

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'min:3', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'max:16', 'confirmed'],
             'empresa' => ['required', 'string', 'min:3', 'max:255', 'unique:tenants,name'],
-            'cnpj' => ['required', 'numeric', 'min:14', 'max:14', 'unique:tenants'],
+            'cnpj' => ['required', 'numeric', 'digits:14', 'unique:tenants'],
         ]);
     }
 
