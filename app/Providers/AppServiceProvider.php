342a62313465aa99199f27af35e40d40bea3e3af
Carlos Ferreira <conta@especializati.com.br>
2020-12-23 10:32:56 -0300

    Upgrade to 8.x from 7.x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,7 @@ use App\Observers\{
     TableObserver,
     TenantObserver
 };
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -40,6 +41,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
+
         Plan::observe(PlanObserver::class);
         Tenant::observe(TenantObserver::class);
         Category::observe(CategoryObserver::class);
--- database/seeders/TenantsTableSeeder.php
+++ database/seeders/TenantsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\{
     Plan,
     Tenant
